e094e71399d1e76ef9bc066e78450db6810a5578
richie <richiewillynton@gmail.com>
2025-07-01 22:50:27 +0800

    mason plugins ensure install

--- .config/nvim/lua/richie/lazy/lsp-config.lua
+++ .config/nvim/lua/richie/lazy/lsp-config.lua
@@ -2,14 +2,21 @@ return {
   "mason-org/mason-lspconfig.nvim",
   opts = {
     -- Ensure clangd is installed via Mason
-    ensure_installed = { "clangd" },
+    ensure_installed = {  
+    "clangd",           -- For C/C++ language server  
+    "clang-format",     -- For C/C++ code formatting  
+    "cmake-language-server",  
+    "gitleaks",  
+    "lua-language-server",  
+    "ocamlformat",  
+     },  
   },
   dependencies = {
     { "mason-org/mason.nvim", opts = {} },
     "neovim/nvim-lspconfig",
   },
   config = function()
-    -- Set up clangd with formatting capabilities
+    -- Set up clangd 
     require("lspconfig").clangd.setup({
       capabilities = require("cmp_nvim_lsp").default_capabilities(),
       on_attach = function(client, bufnr)
