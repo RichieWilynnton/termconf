8944ee7cef36f6defadcd3940cf30370f59b17cf
richie <richiewillynton@gmail.com>
2025-07-03 23:06:08 +0800

    c-... copies to register now

--- .config/nvim/lua/richie/remap-keys.lua
+++ .config/nvim/lua/richie/remap-keys.lua
@@ -3,17 +3,17 @@ vim.g.mapleader = " "
 --Make d not copy to clipboard in visual mode 
 vim.keymap.set('v', 'd', '"_d', { noremap = true })
 
-vim.keymap.set('n', 'cw', '"_cw', { noremap = true })
-vim.keymap.set('n', 'ciw', '"_ciw', { noremap = true })
-vim.keymap.set('n', 'ci"', '"_ci"', { noremap = true })
-vim.keymap.set('n', "ci'", '"_ci\'', { noremap = true })
-vim.keymap.set('n', 'ci[', '"_ci[', { noremap = true })
-vim.keymap.set('n', 'ci]', '"_ci]', { noremap = true })
-vim.keymap.set('n', 'ci(', '"_ci(', { noremap = true })
-vim.keymap.set('n', 'ci)', '"_ci)', { noremap = true })
-vim.keymap.set('n', 'ci{', '"_ci{', { noremap = true })
-vim.keymap.set('n', 'ci}', '"_ci}', { noremap = true })
-vim.keymap.set('n', 'cc', '"_cc', { noremap = true })
+-- vim.keymap.set('n', 'cw', '"_cw', { noremap = true })
+-- vim.keymap.set('n', 'ciw', '"_ciw', { noremap = true })
+-- vim.keymap.set('n', 'ci"', '"_ci"', { noremap = true })
+-- vim.keymap.set('n', "ci'", '"_ci\'', { noremap = true })
+-- vim.keymap.set('n', 'ci[', '"_ci[', { noremap = true })
+-- vim.keymap.set('n', 'ci]', '"_ci]', { noremap = true })
+-- vim.keymap.set('n', 'ci(', '"_ci(', { noremap = true })
+-- vim.keymap.set('n', 'ci)', '"_ci)', { noremap = true })
+-- vim.keymap.set('n', 'ci{', '"_ci{', { noremap = true })
+-- vim.keymap.set('n', 'ci}', '"_ci}', { noremap = true })
+-- vim.keymap.set('n', 'cc', '"_cc', { noremap = true })
 
 vim.keymap.set('n', '<leader>gg', ':CopilotChatToggle<CR>', { noremap = true, silent = true })
 
